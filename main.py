771bc54c99fd26980176a33f11cce5b7f8b2651c
mehmetx42 <144047609+mehmetx42@users.noreply.github.com>
2023-09-06 12:03:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,23 @@ meme_dict = {
     "GG": "İyi oyundu anlamında verilen cevap",
     "NT": "İyi denemeydi anlamında verilen cevap",
     "GANG": "Yardım anlamında verilen cevap",
+    "ROFL" - "bir şakaya karşılık cevap",
+    "SHEESH" - "onaylamamak",
+    "CREEPY" - "korkunç",
+    "AGGRO" - "agresifleşmek/sinirlenmek",
+    "IF" - "eğer",
+    "EZ" - "kolay/kolaydı",
+    "U2" - "seninde/sende",
+    "HERO" - "Karakter",
+    "HP" - "Can",
+    "HF" - "İyi eğlenceler",
+    "HEAL" - "Can verme",
+    "AUTOFİLL" - "Tercih dışı seçim",
+    "ACE" - "Katliam",
+    "Ability" - "Oyundaki şampiyonların yetenekleri",
+    "AFK" - "Oyunu bırakıp farklı bir yere gitmek, oyun ortasında bilgisayardan kalkmak",
 }
+
 for dongu in range(5):
     
     word = input("Anlamadığınız bir kelime yazın (hepsini büyük harflerle yazın!): ")
